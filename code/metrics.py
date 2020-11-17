35f3fc5da65edec4ee0a2db974b7169f56196f1a
charles.gulian <charles.gulian@ethree.com>
2020-11-17 15:21:18 -0500

    Added new metrics (max_exceeding and reserve_ramp_rate) to metrics.py.

--- code/metrics.py
+++ code/metrics.py
@@ -11,3 +11,10 @@ def exceeding(y_true, y_pred):
 
 def closeness(y_true, y_pred):
     return np.mean(np.abs(y_true - y_pred))
+
+def max_exceeding(y_true, y_pred):
+    return np.max(y_true - y_pred)
+
+def reserve_ramp_rate(y_true, y_pred):
+    dt = 0.25 # hours (interval between time periods)
+    return np.mean([np.abs(y_pred[i] - y_pred[i-1]) for i in range(1,len(y_pred))])/dt
