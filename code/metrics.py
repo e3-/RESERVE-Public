20bb140a75f4c7aed3867aaff488a55c11b168b2
charles.gulian <charles.gulian@ethree.com>
2020-11-18 10:29:06 -0500

    Added "n_crossings" (quantile crossing check) to metrics.py.

--- code/metrics.py
+++ code/metrics.py
@@ -18,3 +18,7 @@ def max_exceeding(y_true, y_pred):
 def reserve_ramp_rate(y_true, y_pred):
     dt = 0.25 # hours (interval between time periods)
     return np.mean([np.abs(y_pred[i] - y_pred[i-1]) for i in range(1,len(y_pred))])/dt
+
+def n_crossings(y_pred1, y_pred2):
+    # y_pred1 is lower quantile estimate; y_pred2 is upper quantile estimate (crossing when y_pred1 > y_pred2)
+    return np.sum(y_pred1 > y_pred2)
