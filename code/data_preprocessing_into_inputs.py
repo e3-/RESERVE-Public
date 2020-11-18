d323308c92edd642b5bc14182f83d6040b74b6eb
YuchiSun <sunyuchi_@live.com>
2020-11-18 14:11:25 -0800

    Bug fix: Recursively create parent dirs (#11)
    * Separate raw data and input data. Input data for different version of the model lives in different folders
    
    Minor: Refactor data_preprocessing_into_inputs.py for running as a script
    
    * Correction for raw data location.
    
    * Use makedirs instead of mkdir to recursively create parent directory.

--- code/data_preprocessing_into_inputs.py
+++ code/data_preprocessing_into_inputs.py
@@ -1,6 +1,6 @@
-#==== TODO: Consider having option to disallow overlaps between subsequent training samples
+# ==== TODO: Consider having option to disallow overlaps between subsequent training samples
 
-#==== Change log v1.2 (11/07/2020) ====
+# ==== Change log v1.2 (11/07/2020) ====
 # Switched to autogeneration of column names in the M by N matrix based on lag and feature name
 # Transposed the M*N matrix in the output. Time is in index and different features are in column
 # Move datetime into the index of the input and output dataframe
@@ -8,7 +8,7 @@
 # Allow to not include a feature by inputting lag_term_start> lag_term_end
 # Streamline the lag term start and end list into a dictionary for further flexibility
 
-#==== Change log v1.1 (11/01/2020) ====
+# ==== Change log v1.1 (11/01/2020) ====
 
 # Updated to better facilitate future changes in what M would be - both the broad types of predictors and # of lag
 # terms corresponding to each
@@ -18,16 +18,19 @@
 # Now, TRUE = Good, FALSE = Bad -> Based on change in data cleaning upstream
 # Some efficiency updates
 
-#====V1.0 originally created (10/22/2020) ====
+# ====V1.0 originally created (10/22/2020) ====
 
 import os
 import numpy as np
 import pandas as pd
 import utility
 
-#==== User inputs ====
+# ==== User inputs ====
+# the name of the model version that this data would serve
+model_name = "rescue_v1_1_no_calendar"
+
 # Define the amount of lag terms that would end up in the input for each feature type
-# +1->Forecast time, 0->Present time, -1->1 time step in past, -2->2 time steps in past... 
+# +1->Forecast time, 0->Present time, -1->1 time step in past, -2->2 time steps in past...
 # E.g. 1: start = -2, end = -1 implies only include values from 2 past time steps.
 # E.g. 2: start = 0 , end = -1 implies do not include any terms for this feature.
 lag_term_start_predictors = np.array([-2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2])
@@ -40,21 +43,8 @@ response_lead_term = 1  # As a gentle reminder, its relative to present time, T0
 longitude = -119.4179  # Roughly passing through the center of CA
 time_difference_from_UTC = -8  # hours. Timestamps for input data are in PST
 
-# Labels for response (output model is trained to predict)
-response_col_name = "Net_Load_Forecast_Error"
 
-
-#==== Constants for use in script that DON'T need to be user defined ====
-# Paths to read raw data files from and to store outputs in. Defined in the dir_structure class in utility1
-dir_str = utility.Dir_Structure()
-
-# The names of serveral calendar related terms
-hour_angle_col_name = "Hour_Angle"
-day_angle_col_name = "Day_Angle"
-days_from_start_date_col_name = "Days_from_Start_Date"
-
-
-#==== Helper functions that don't need user intervention ====
+# ==== Helper functions that don't need user intervention ====
 # User needs to define what the response variable is
 def calculate_response_variable(raw_data_df):
     """
@@ -172,77 +162,96 @@ def pad_raw_data_w_lag_lead(raw_data_df, lag_term_start_predictors, lag_term_end
     return raw_data_df, raw_data_start_idx, raw_data_end_idx
 
 
-#==== 1. Reading in raw data and validate/modify the data for downstream manipulation ====
+def main(model_name=model_name, lag_term_start_predictors=lag_term_start_predictors,
+         lag_term_end_predictors=lag_term_end_predictors,response_lead_term = response_lead_term,
+         longitude = longitude, time_difference_from_UTC = time_difference_from_UTC):
 
-# Read in raw data to be used to create predictors and response variables
-raw_data_df = pd.read_csv(dir_str.raw_data_path, index_col=0, parse_dates=True, infer_datetime_format=True)
-raw_data_validity = pd.read_csv(dir_str.raw_data_validity_path, index_col=0, parse_dates=True, infer_datetime_format=True)
-
-# Check the validity mask of the raw data is consistent with data's shape
-assert (raw_data_df.index == raw_data_validity.index).all(), "Validity mask and Data index inconsistent!"
-assert (raw_data_df.columns == raw_data_validity.columns).all(), "Validity mask and Data fields inconsistent!"
-
-# Embed info about validity into df holding values so we can use the latter alone going forward
-raw_data_df[~raw_data_validity] = None
-num_time_points = raw_data_df.shape[0]
-
-# Pad the raw data with NaNs in both the lag and lead direction for downstream data manipulation
-raw_data_df, raw_data_start_idx, raw_data_end_idx = pad_raw_data_w_lag_lead(raw_data_df, lag_term_start_predictors,
-                                                                            lag_term_end_predictors, response_lead_term)
-raw_data_start_date = raw_data_df.index[raw_data_start_idx]
+    # ==== Constants for use in script that DON'T need to be user defined ====
+    # Labels for response (output model is trained to predict)
+    response_col_name = "Net_Load_Forecast_Error"
+    # The names of several calendar related terms
+    hour_angle_col_name = "Hour_Angle"
+    day_angle_col_name = "Day_Angle"
+    days_from_start_date_col_name = "Days_from_Start_Date"
 
 
-#==== 2. Add in calendar terms for the raw data ====
-print("Calculating calendar-based predictors....")
-raw_data_df[hour_angle_col_name], raw_data_df[day_angle_col_name], raw_data_df[days_from_start_date_col_name] = \
-    calculate_calendar_based_predictors(raw_data_df.index, longitude, time_difference_from_UTC, raw_data_start_date)
+    # ==== 1. Reading in raw data and validate/modify the data for downstream manipulation ====
+    # Paths to read raw data files from and to store outputs in. Defined in the dir_structure class in utility
+    dir_str = utility.Dir_Structure(model_name=model_name)
+
+    # Read in raw data to be used to create predictors and response variables
+    raw_data_df = pd.read_csv(dir_str.raw_data_path, index_col=0, parse_dates=True, infer_datetime_format=True)
+    raw_data_validity = pd.read_csv(dir_str.raw_data_validity_path, index_col=0, parse_dates=True,
+                                    infer_datetime_format=True)
+
+    # Check the validity mask of the raw data is consistent with data's shape
+    assert (raw_data_df.index == raw_data_validity.index).all(), "Validity mask and Data index inconsistent!"
+    assert (raw_data_df.columns == raw_data_validity.columns).all(), "Validity mask and Data fields inconsistent!"
+
+    # Embed info about validity into df holding values so we can use the latter alone going forward
+    raw_data_df[~raw_data_validity] = None
+    num_time_points = raw_data_df.shape[0]
+
+    # Pad the raw data with NaNs in both the lag and lead direction for downstream data manipulation
+    raw_data_df, raw_data_start_idx, raw_data_end_idx = pad_raw_data_w_lag_lead(raw_data_df, lag_term_start_predictors,
+                                                                                lag_term_end_predictors,
+                                                                                response_lead_term)
+    raw_data_start_date = raw_data_df.index[raw_data_start_idx]
+
+    # ==== 2. Add in calendar terms for the raw data ====
+    print("Calculating calendar-based predictors....")
+    raw_data_df[hour_angle_col_name], raw_data_df[day_angle_col_name], raw_data_df[days_from_start_date_col_name] = \
+        calculate_calendar_based_predictors(raw_data_df.index, longitude, time_difference_from_UTC, raw_data_start_date)
+
+    # ==== 3. Add in netload forecast difference for the raw data ====
+    print("Calculating response....")
+    raw_data_df[response_col_name] = calculate_response_variable(raw_data_df)
+
+    # Revise the lag term array since we are extending the original data
+    num_feature_ext = len(raw_data_df.columns) - len(lag_term_start_predictors)
+    lag_term_start_predictors = np.hstack(
+        (lag_term_start_predictors, np.ones(num_feature_ext, dtype=int) * response_lead_term))
+    lag_term_end_predictors = np.hstack(
+        (lag_term_end_predictors, np.ones(num_feature_ext, dtype=int) * response_lead_term))
+
+    # ==== 4. Using vectorized operations to construct lag terms ====
+    print("Creating trainval samples for all time-points ....")
+    # Initialize collectors to hold (and later save) trainval data in
+    trainval_data_df = pd.DataFrame(None, index=raw_data_df.index[raw_data_start_idx:raw_data_end_idx])
+
+    # Colelct lag term predictors for all trainval samples
+    # Iterate over each type of lag term predictor
+    for feature_idx, feature_type in enumerate(raw_data_df.columns):
+
+        # obtain lag term start and end offset for a certain feature
+        lag_term_start = lag_term_start_predictors[feature_idx]
+        lag_term_end = lag_term_end_predictors[feature_idx]
+
+        # Iterate over each time step for current predictor type
+        for time_step in range(lag_term_start, lag_term_end + 1):
+            label = "{}_T{:+}".format(feature_type, time_step)
+            trainval_data_df[label] = (raw_data_df[feature_type]
+                                       .iloc[raw_data_start_idx + time_step: raw_data_end_idx + time_step].values)
+
+    # ==== 5. Drop invalid terms and store to hard drive ====
+    # Identify trainval samples wherein all lag term of features and responses are valid
+    # If any entry is pd.NA, it is invalid
+    print("{} of {} trainval samples are valid"
+          .format(trainval_data_df.notna().all(axis=1).sum(), trainval_data_df.shape[0]))
+    print("Proceeding to delete the rest....")
+    trainval_data_df = trainval_data_df.dropna()
+
+    # Only retain trainval samples wherein predictor(s) and response are both valid
+    output_idx = np.where([response_col_name in col for col in trainval_data_df.columns])[0][0]
+    trainval_outputs_df = trainval_data_df.pop(trainval_data_df.columns[output_idx])
+
+    # Save trainval samples
+    print("Saving files......")
+    trainval_data_df.to_pickle(dir_str.input_trainval_path)
+    trainval_outputs_df.to_pickle(dir_str.output_trainval_path)
+    print("All done!")
 
 
-#==== 3. Add in netload forecast difference for the raw data ====
-print("Calculating response....")
-raw_data_df[response_col_name] = calculate_response_variable(raw_data_df)
-
-# Revise the lag term array since we are extending the original data
-num_feature_ext = len(raw_data_df.columns) - len(lag_term_start_predictors)
-lag_term_start_predictors = np.hstack(
-    (lag_term_start_predictors, np.ones(num_feature_ext, dtype=int) * response_lead_term))
-lag_term_end_predictors = np.hstack((lag_term_end_predictors, np.ones(num_feature_ext, dtype=int) * response_lead_term))
-
-
-#==== 4. Using vectorized operations to construct lag terms ====
-print("Creating trainval samples for all time-points ....")
-# Initialize collectors to hold (and later save) trainval data in
-trainval_data_df = pd.DataFrame(None, index=raw_data_df.index[raw_data_start_idx:raw_data_end_idx])
-
-# Colelct lag term predictors for all trainval samples
-# Iterate over each type of lag term predictor
-for feature_idx, feature_type in enumerate(raw_data_df.columns):
-
-    # obtain lag term start and end offset for a certain feature
-    lag_term_start = lag_term_start_predictors[feature_idx]
-    lag_term_end = lag_term_end_predictors[feature_idx]
-
-    # Iterate over each time step for current predictor type
-    for time_step in range(lag_term_start, lag_term_end + 1):
-        label = "{}_T{:+}".format(feature_type, time_step)
-        trainval_data_df[label] = (raw_data_df[feature_type]
-                                   .iloc[raw_data_start_idx + time_step: raw_data_end_idx + time_step].values)
-
-        
-#==== 5. Drop invalid terms and store to hard drive ====
-# Identify trainval samples wherein all lag term of features and responses are valid
-# If any entry is pd.NA, it is invalid
-print("{} of {} trainval samples are valid"
-      .format(trainval_data_df.notna().all(axis=1).sum(), trainval_data_df.shape[0]))
-print("Proceeding to delete the rest....")
-trainval_data_df = trainval_data_df.dropna()
-
-# Only retain trainval samples wherein predictor(s) and response are both valid
-output_idx = np.where([response_col_name in col for col in trainval_data_df.columns])[0][0]
-trainval_outputs_df = trainval_data_df.pop(trainval_data_df.columns[output_idx])
-
-# Save trainval samples
-print("Saving files......")
-trainval_data_df.to_pickle(dir_str.input_trainval_path)
-trainval_outputs_df.to_pickle(dir_str.output_trainval_path)
-print("All done!")
+# run as a script
+if __name__ == '__main__':
+    main()
--- code/utility.py
+++ code/utility.py
@@ -1,10 +1,11 @@
 import os
 import shutil
 
+
 class Dir_Structure:
     """Directory and file structure of the RESCUE model."""
 
-    def __init__(self, code_dir = os.getcwd(), model_name = 'rescue'):
+    def __init__(self, code_dir=os.getcwd(), model_name='rescue'):
         """Initialize directory structure based on scenario name
         Args:
             code_directory (str): Path to `code` directory where all python code is located
@@ -12,35 +13,35 @@ class Dir_Structure:
         """
         self.model_name = model_name
         self.code_dir = code_dir
-        
+
         # Define paths to directories
-        self.par_dir = os.path.dirname(self.code_dir) # parent directory
-        self.data_dir = os.path.join(self.par_dir, "data") # stores data/input
-        self.output_dir = os.path.join(self.par_dir, "output", self.model_name) # stores inferrence results
-        self.logs_dir = os.path.join(self.par_dir, "logs", self.model_name) # training log for tensorboard
-        self.ckpts_dir = os.path.join(self.par_dir, "ckpts", self.model_name) # check points for accidental pauses
-        self.models_dir = os.path.join(self.par_dir, "models", self.model_name) # trained models are stored here
-        self.diag_dir = os.path.join(self.par_dir, "diagnostics", self.model_name) # graphs and diagnostics
-        
+        self.par_dir = os.path.dirname(self.code_dir)  # parent directory
+        self.raw_data_dir = os.path.join(self.par_dir, "data", "raw_data")
+        self.data_dir = os.path.join(self.par_dir, "data", self.model_name)  # stores data/input
+        self.output_dir = os.path.join(self.par_dir, "output", self.model_name)  # stores inferrence results
+        self.logs_dir = os.path.join(self.par_dir, "logs", self.model_name)  # training log for tensorboard
+        self.ckpts_dir = os.path.join(self.par_dir, "ckpts", self.model_name)  # check points for accidental pauses
+        self.models_dir = os.path.join(self.par_dir, "models", self.model_name)  # trained models are stored here
+        self.diag_dir = os.path.join(self.par_dir, "diagnostics", self.model_name)  # graphs and diagnostics
+
         # Define paths to files
-        self.shuffled_indices_path = os.path.join(self.data_dir,"shuffled_indices_{}.npy".format(self.model_name))
-        self.input_trainval_path = os.path.join(self.data_dir,"input_trainval.pkl")
-        self.output_trainval_path = os.path.join(self.data_dir,"output_trainval.pkl")
-        self.raw_data_path = os.path.join(self.data_dir,"input_values_for_M_by_N_creating_script.csv")
-        self.raw_data_validity_path = os.path.join(self.data_dir,"input_validity_flags_for_M_by_N_creating_script.csv")
-        self.pred_trainval_path = os.path.join(self.output_dir,"pred_trainval.pkl")
+        self.shuffled_indices_path = os.path.join(self.data_dir, "shuffled_indices_{}.npy".format(self.model_name))
+        self.input_trainval_path = os.path.join(self.data_dir, "input_trainval.pkl")
+        self.output_trainval_path = os.path.join(self.data_dir, "output_trainval.pkl")
+        self.raw_data_path = os.path.join(self.raw_data_dir, "input_values_for_M_by_N_creating_script.csv")
+        self.raw_data_validity_path = os.path.join(self.raw_data_dir, "input_validity_flags_for_M_by_N_creating_script.csv")
+        self.pred_trainval_path = os.path.join(self.output_dir, "pred_trainval.pkl")
         self.training_hist_path = os.path.join(self.diag_dir, "training_history.npy")
-        
+
         # clear all contents in the log directory
-        shutil.rmtree(self.logs_dir)
-        
+        if os.path.exists(self.logs_dir):
+            shutil.rmtree(self.logs_dir)
+
         # make these directories if they do not already exist
         self.make_directories()
-        
-
-        
 
     def make_directories(self):
-        for folder in [self.data_dir, self.output_dir, self.logs_dir, self.ckpts_dir, self.models_dir, self.diag_dir]:
+        for folder in [self.data_dir, self.raw_data_dir, self.output_dir,
+                       self.logs_dir, self.ckpts_dir, self.models_dir, self.diag_dir]:
             if not os.path.exists(folder):
-                os.mkdir(folder)
+                os.makedirs(folder)
