431ca452b6cc6a9e81ddf6159dd21612d5163ef0
JCStevens2018 <43827059+JCStevens2018@users.noreply.github.com>
2021-08-05 17:04:36 -0700

    added softare license
    added software license as commented block in header, does not affect functionality.

--- code/utility.py
+++ code/utility.py
@@ -1,6 +1,26 @@
+# ############################ LICENSE INFORMATION ############################
+# This file is part of the E3 RESERVE Model.
+
+# Copyright (C) 2021 Energy and Environmental Economics, Inc.
+# For contact information, go to www.ethree.com
+
+# The E3 RESERVE Model is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# The E3 RESERVE Model is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with the E3 RESERVE Model (in the file LICENSE.TXT). If not,
+# see <http://www.gnu.org/licenses/>.
+# #############################################################################
+
 import os
 import shutil
-
 
 class Dir_Structure:
     """Directory and file structure of the RESCUE model."""
